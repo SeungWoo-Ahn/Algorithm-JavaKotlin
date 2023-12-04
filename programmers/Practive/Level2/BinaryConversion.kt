1beeda6db50400ed69c9f61ef4dce21f29d876da
rio319-an <78468001+rio319-an@users.noreply.github.com>
2023-12-04 17:03:05 +0900

    이진 변환 반복하기

---
+++ programmers/Practive/Level2/BinaryConversion.kt
@@ -0,0 +1,38 @@
+package programmers.Practive.Level2
+
+import java.lang.StringBuilder
+
+class BinaryConversion {
+
+    var conversionCnt = 0
+    var zeroCnt = 0
+
+    fun solution(s: String): IntArray {
+        var input = s
+        while (input != "1") {
+            input = filterZero(input)
+            input = makeBinary(input)
+        }
+        return intArrayOf(conversionCnt, zeroCnt)
+    }
+
+    private fun filterZero(s: String): String {
+        val sb = StringBuilder()
+        for(index in s.indices) {
+            if (s[index] == '0') zeroCnt++
+            else sb.append(s[index])
+        }
+        return sb.toString()
+    }
+
+    private fun makeBinary(s: String): String {
+        val len = s.length
+        conversionCnt++
+        return len.toString(2)
+    }
+}
+
+fun main() {
+    println(BinaryConversion().solution("110010101001").contentToString())
+}
+
