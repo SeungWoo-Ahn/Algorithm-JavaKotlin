fc7630ee8431ed23d4372a004eab70617b4a7588
rio319-an <78468001+rio319-an@users.noreply.github.com>
2023-12-04 19:08:01 +0900

    귤 고르기

---
+++ programmers/Practive/Level2/SelectTangerine.kt
@@ -0,0 +1,28 @@
+package programmers.Practive.Level2
+
+import java.util.Arrays
+
+class SelectTangerine {
+    fun solution(k: Int, tangerine: IntArray): Int {
+        var answer = 0
+        val maxSize = tangerine.maxOf { it }
+        val t = IntArray(maxSize+1)
+        for (i in tangerine.indices) {
+            t[tangerine[i]]++
+        }
+        Arrays.sort(t)
+        var total = 0
+        for (i in t.size - 1 downTo 0) {
+            total += t[i]
+            answer++
+            if (total >= k) break
+        }
+        return answer
+    }
+}
+
+fun main() {
+    val k = 6
+    val tangerine = intArrayOf(1, 3, 2, 5, 4, 5, 2, 3)
+    println(SelectTangerine().solution(k, tangerine))
+}
