2b36b2d9a54512ad9accd46edcc3a67c049ee4c5
rio319-an <78468001+rio319-an@users.noreply.github.com>
2023-12-04 18:39:20 +0900

    멀리 뛰기

---
+++ programmers/Practive/Level2/LongJump.kt
@@ -0,0 +1,23 @@
+package programmers.Practive.Level2
+
+class LongJump {
+
+    fun solution(n: Int): Long {
+        if (n == 1) return 1
+        if (n == 2) return 2
+        val d = LongArray(n+1).apply {
+            this[0] = 0L
+            this[1] = 1L
+            this[2] = 2L
+        }
+        for (i in 3..n) {
+            d[i] = (d[i-1] + d[i-2]) % 1234567
+        }
+        return d[n]
+    }
+}
+
+fun main() {
+    println(LongJump().solution(4))
+}
+
