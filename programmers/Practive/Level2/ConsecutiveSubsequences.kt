7f5370905c62cafa329f38af34196803b174f5bc
rio319-an <78468001+rio319-an@users.noreply.github.com>
2023-12-04 20:46:21 +0900

    연속 부분 수열 합의 개수

---
+++ programmers/Practive/Level2/ConsecutiveSubsequences.kt
@@ -0,0 +1,30 @@
+package programmers.Practive.Level2
+
+class ConsecutiveSubsequences {
+    fun solution(elements: IntArray): Int {
+        val set = HashSet<Int>()
+        for (len in 1..elements.size) {
+            for (i in elements.indices) {
+                set.add(sumByLen(elements, len, i))
+            }
+        }
+        return set.size
+    }
+
+    private fun sumByLen(elements: IntArray, len: Int, i: Int): Int {
+        var cnt = 0
+        var index = i
+        var sum = 0
+        while (cnt < len) {
+            sum += elements[index]
+            cnt++
+            index = if (index + 1 == elements.size) 0 else index + 1
+        }
+        return sum
+    }
+}
+
+fun main() {
+    val elements = intArrayOf(7,9,1,1,4)
+    println(ConsecutiveSubsequences().solution(elements))
+}
