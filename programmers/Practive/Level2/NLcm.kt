0b58147f0e3bda7b8fde57adf2b6bb243029800e
rio319-an <78468001+rio319-an@users.noreply.github.com>
2023-12-04 17:25:31 +0900

    N개의 최소공배수

---
+++ programmers/Practive/Level2/NLcm.kt
@@ -0,0 +1,27 @@
+package programmers.Practive.Level2
+
+class NLcm {
+    fun solution(arr: IntArray): Int {
+        var answer = 1
+        while (true) {
+            val isLcm = isLcm(arr, answer)
+            if (isLcm) break
+            else answer++
+        }
+        return answer
+    }
+
+    private fun isLcm(arr: IntArray, predict: Int): Boolean {
+        for (index in arr.indices) {
+            if (predict % arr[index].toDouble() != 0.0) {
+                return false
+            }
+        }
+        return true
+    }
+}
+
+fun main() {
+    val arr = intArrayOf(2,6,8,14)
+    println(NLcm().solution(arr))
+}
