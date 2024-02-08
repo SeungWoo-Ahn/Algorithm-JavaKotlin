7eccdc6d05106c2cb73f701f52bcc3c4c0789537
rio319-an <78468001+rio319-an@users.noreply.github.com>
2024-02-08 20:11:29 +0900

    N과 M (6) 15655

---
+++ boj/backtracking/15655.kt
@@ -0,0 +1,35 @@
+package boj.backtracking
+
+import java.lang.StringBuilder
+import java.util.StringTokenizer
+
+class `15655` {
+    private var input = intArrayOf()
+    private var arr = intArrayOf()
+    private val sb = StringBuilder()
+
+    private fun backtracking(n: Int, m: Int, depth: Int, startIdx: Int) {
+        if (depth == m) {
+            arr.forEach { sb.append("$it ") }
+            sb.append("\n")
+            return
+        }
+        for (i in startIdx until n) {
+            arr[depth] = input[i]
+            backtracking(n, m, depth + 1, i + 1)
+        }
+    }
+
+    fun solution() = with(System.`in`.bufferedReader()) {
+        val (N, M) = readLine().split(" ").map { it.toInt() }
+        val st = StringTokenizer(readLine())
+        input = IntArray(N) { st.nextToken().toInt() }.apply { sort() }
+        arr = IntArray(M)
+        backtracking(N, M, 0, 0)
+        println(sb)
+    }
+}
+
+fun main() {
+    `15655`().solution()
+}
