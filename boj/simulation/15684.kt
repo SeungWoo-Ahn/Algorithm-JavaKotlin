82cfc28c11d4f7351f7e79a59341d97662c9e24a
안승우 <rio319@naver.com>
2024-02-19 21:31:55 +0900

    (개선) 사다리 조작 15684

--- boj/simulation/15684.kt
+++ boj/simulation/15684.kt
@@ -1,91 +1,70 @@
 package boj.simulation
 
-import kotlin.math.min
+import java.util.StringTokenizer
 
 class `15684` {
-    data class Node(val x: Int, val y: Int)
-
+    private var n = 0
+    private var m = 0
+    private var h = 0
+    private var minCnt = 0
     private var map: Array<BooleanArray> = arrayOf()
-    private val canPut = arrayListOf<Node>()
-    private val arr = IntArray(3)
-    private var minCnt = 4
 
-    private fun findCanPut() {
-        for (i in map.indices) {
-            for (j in map[0].indices) {
-                if (map[i][j]) continue
-                if (map[0].size == 1) {
-                    canPut.add(Node(i, j))
+    private fun dfs(depth: Int, max: Int) {
+        if (depth == max) {
+            if (isCorrect()) {
+                minCnt = max
+            }
+            return
+        }
+
+        for (j in 1 .. n) {
+            var i = 1
+            while (i <= h) {
+                if (map[i][j] || map[i][j - 1] || map[i][j + 1]){
+                    i++
                     continue
                 }
-                when (j) {
-                    0 -> if (!map[i][1]) canPut.add(Node(i, j))
-                    map[0].size - 1 -> if (!map[i][j - 1]) canPut.add(Node(i, j))
-                    else -> if (!map[i][j - 1] && !map[i][j + 1]) canPut.add(Node(i, j))
-                }
+                map[i][j] = true
+                dfs(depth + 1, max)
+                map[i][j] = false
+
+                while (i <= h && !map[i][j - 1] && !map[i][j + 1]) i++
             }
         }
     }
 
-    private fun backtracking(depth: Int, startIdx: Int) {
-        val gameMap = Array(map.size) { i -> map[i].copyOf() }
-        if (canPlay(gameMap, depth)) play(gameMap, depth)
-        if (depth == min(3, minCnt)) return
-
-        for (i in startIdx until canPut.size) {
-            arr[depth] = i
-            backtracking(depth + 1, i + 1)
-        }
-    }
-
-    private fun canPlay(gameMap: Array<BooleanArray>, depth: Int): Boolean {
-        for (i in 0 until depth) {
-            gameMap[canPut[arr[i]].x][canPut[arr[i]].y] = true
-        }
-        if (gameMap[0].size == 1) return true
-        for (i in gameMap.indices) {
-            for (j in 0 until gameMap[0].size - 1) {
-                if (gameMap[i][j] && gameMap[i][j + 1]) return false
+    private fun isCorrect(): Boolean {
+        for (j in 1 .. n) {
+            var col = j
+            for (i in 1 .. h) {
+                if (map[i][col]) col++
+                else if (map[i][col - 1]) col--
             }
+            if (col != j) return false
         }
         return true
     }
 
-    private fun play(gameMap: Array<BooleanArray>, depth: Int) {
-        var isCorrect = true
-        for (j in 0 ..  gameMap[0].size) {
-            var curCol = j
-            for (i in gameMap.indices) {
-                when (curCol) {
-                    0 -> if (gameMap[i][curCol]) curCol++
-                    gameMap[0].size -> if (gameMap[i][curCol - 1]) curCol--
-                    else -> {
-                        if (gameMap[i][curCol]) curCol++
-                        else if (gameMap[i][curCol - 1]) curCol--
-                    }
-                }
-            }
-            if (curCol != j) {
-                isCorrect = false
-                break
-            }
-        }
-        if (isCorrect && depth < minCnt) {
-            minCnt = depth
-        }
-    }
-
     fun solution() = with(System.`in`.bufferedReader()) {
-        val (n, m, h) = readLine().split(" ").map { it.toInt() }
-        map = Array(h) { BooleanArray(n - 1) }
+        val st = StringTokenizer(readLine())
+        n = st.nextToken().toInt()
+        m = st.nextToken().toInt()
+        h = st.nextToken().toInt()
+        map = Array(h + 2) { BooleanArray(n + 2) }
+
         repeat(m) {
             val (a, b) = readLine().split(" ").map { it.toInt() }
-            map[a - 1][b - 1] = true
+            map[a][b] = true
         }
-        findCanPut()
-        backtracking(0, 0)
-        if (minCnt == 4) println(-1)
-        else println(minCnt)
+
+        for (i in 0 .. 3) {
+            dfs(0, i)
+            if (minCnt != -1) {
+                println(minCnt)
+                return
+            }
+        }
+        println(-1)
     }
 }
 
