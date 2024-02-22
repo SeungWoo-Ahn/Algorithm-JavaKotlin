0c6f8c3379a521e6590c37ca4415ad55368e85b1
안승우 <rio319@naver.com>
2024-02-22 19:58:20 +0900

    (개선) 계단 오르기 2579

--- boj/dp/2579.kt
+++ boj/dp/2579.kt
@@ -1,24 +1,30 @@
 package boj.dp
 
-import kotlin.math.max
+import kotlin.math.min
 
 class `2579` {
     fun solution() = with(System.`in`.bufferedReader()) {
         val n = readLine().toInt()
         val score = IntArray(n + 1)
-        repeat(n) { score[it + 1] = readLine().toInt() }
-        if (n == 1) {
-            println(score[1])
+        var total = 0
+        repeat(n) { i ->
+            readLine().toInt().let {
+                score[i + 1] = it
+                total += it
+            }
+        }
+        if (n <= 2) {
+            println(total)
             return
         }
-        val d = Array(n + 1) { IntArray(3)  }
-        d[1][1] = score[1].also { d[1][2] = 0 }
-        d[2][1] = score[2].also { d[2][2] = score[1] + score[2] }
-        for (i in 2 .. n) {
-            d[i][1] = max(d[i - 2][1], d[i - 2][2]) + score[i]
-            d[i][2] = d[i - 1][1] + score[i]
+        val d = IntArray(n + 1)
+        d[1] = score[1]
+        d[2] = score[2]
+        d[3] = score[3]
+        for (i in 4 until n) {
+            d[i] = min(d[i - 2], d[i - 3]) + score[i]
         }
-        println(max(d[n][1], d[n][2]))
+        println(total - min(d[n - 1], d[n - 2]))
     }
 }
 
