b1161c3a960bc661cf0e1c265df3bb578ab3cc50
안승우 <rio319@naver.com>
2024-05-25 16:03:31 +0900

    도시 분할 계획 1647 (크루스칼)

--- boj/minimum_spanning_tree/1647.kt
+++ boj/minimum_spanning_tree/1647.kt
@@ -3,34 +3,41 @@ package boj.minimum_spanning_tree
 import java.util.PriorityQueue
 
 class `1647` {
+    private var parent = intArrayOf()
+
+    private fun find(x: Int): Int {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x])
+        }
+        return parent[x]
+    }
+
+    private fun union(x: Int, y: Int): Boolean {
+        val rootX = find(x)
+        val rootY = find(y)
+        if (rootX == rootY) return false
+        if (rootX < rootY) parent[rootY] = rootX
+        else parent[rootX] = rootY
+        return true
+    }
+
     fun solution() = with(System.`in`.bufferedReader()) {
         val (n, m) = readLine().splitToInt()
-        val adj = Array(n + 1) { mutableListOf<Pair<Int, Int>>() }
-        val check = BooleanArray(n + 1)
+        val edge = PriorityQueue<Triple<Int, Int, Int>>(compareBy { it.first })
+        parent = IntArray(n + 1) { it }
         repeat(m) {
             val (a, b, cost) = readLine().splitToInt()
-            adj[a].add(cost to b)
-            adj[b].add(cost to a)
+            edge.add(Triple(cost, a, b))
         }
-        val pq = PriorityQueue<Pair<Int, Int>>(compareBy { it.first })
-        adj[1].forEach { pq.add(it) }
-        check[1] = true
         var cnt = 0
         var sum = 0
-        var max = 0
-        while (cnt < n - 1) {
-            val (cost, node) = pq.poll()
-            if (check[node]) continue
-            check[node] = true
+        while (cnt < n - 2) {
+            val (cost, u, v) = edge.poll()
+            if (!union(u, v)) continue
             sum += cost
-            if (cost > max) max = cost
             cnt++
-            for (nxt in adj[node]) {
-                if (!check[nxt.second])
-                    pq.add(nxt)
-            }
         }
-        println(sum - max)
+        println(sum)
     }
 
     private fun String.splitToInt() = split(" ").map { it.toInt() }
