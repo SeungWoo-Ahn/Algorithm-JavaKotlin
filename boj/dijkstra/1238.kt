fd365116b4c181e99ba4954b324c599fd4bb2438
안승우 <rio319@naver.com>
2024-06-02 14:03:14 +0900

    파티 1238 (수정)

--- boj/dijkstra/1238.kt
+++ boj/dijkstra/1238.kt
@@ -4,60 +4,42 @@ import java.util.PriorityQueue
 
 class `1238` {
     private var adj: Array<MutableList<Pair<Int, Int>>> = arrayOf()
-    private var d = intArrayOf()
+    private var adjReversed: Array<MutableList<Pair<Int, Int>>> = arrayOf()
 
-    private fun findToHomeDistance(x: Int) {
+    private fun Array<MutableList<Pair<Int, Int>>>.dijkstra(n: Int, st: Int): IntArray {
+        val d = IntArray(n + 1) { INF }
         val pq = PriorityQueue<Pair<Int, Int>>(compareBy { it.first })
-        d[x] = 0
-        pq.add(d[x] to x)
+        d[st] = 0
+        pq.add(d[st] to st)
         while (pq.isNotEmpty()) {
             val (cost, node) = pq.poll()
             if (d[node] != cost) continue
-            for ((nxtCost, nxtNode) in adj[node]) {
+            for ((nxtCost, nxtNode) in this[node]) {
                 if (d[nxtNode] <= d[node] + nxtCost) continue
                 d[nxtNode] = d[node] + nxtCost
                 pq.add(d[nxtNode] to nxtNode)
             }
         }
-    }
-
-    private fun findToPartyDistance(n: Int, st: Int, en: Int): Int {
-        val distance = IntArray(n + 1) { INF }
-        val pq = PriorityQueue<Pair<Int, Int>>(compareBy { it.first })
-        distance[st] = 0
-        pq.add(distance[st] to st)
-        while (pq.isNotEmpty()) {
-            val (cost, node) = pq.poll()
-            if (distance[node] != cost) continue
-            if (node == en) return cost
-            for ((nxtCost, nxtNode) in adj[node]) {
-                if (distance[nxtNode] <= distance[node] + nxtCost) continue
-                distance[nxtNode] = distance[node] + nxtCost
-                pq.add(distance[nxtNode] to nxtNode)
-            }
-        }
-        return 0
-    }
-
-    private fun findMaxDistance(n: Int, x: Int): Int {
-        var max = 0
-        for (i in 1 .. n) {
-            val distance = d[i] + findToPartyDistance(n, i, x)
-            if (distance > max) max = distance
-        }
-        return max
+        return d
     }
 
     fun solution() = with(System.`in`.bufferedReader()) {
         val (n, m, x) = readLine().splitToInt()
         adj = Array(n + 1) { mutableListOf() }
-        d = IntArray(n + 1) { INF }
+        adjReversed = Array(n + 1) { mutableListOf() }
         repeat(m) {
             val (u, v, t) = readLine().splitToInt()
             adj[u] += t to v
+            adjReversed[v] += t to u
         }
-        findToHomeDistance(x)
-        print(findMaxDistance(n, x))
+        val distanceToHome = adj.dijkstra(n, x)
+        val distanceToParty = adjReversed.dijkstra(n, x)
+        var maxDistance = 0
+        for (i in 1 .. n) {
+            val distance = distanceToHome[i] + distanceToParty[i]
+            if (distance > maxDistance) maxDistance = distance
+        }
+        print(maxDistance)
     }
 
     private fun String.splitToInt() = split(" ").map { it.toInt() }
