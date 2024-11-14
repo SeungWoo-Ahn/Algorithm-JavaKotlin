5b3626dc308d98f09ab37534155793d810c406b8
안승우 <rio319@naver.com>
2024-11-14 20:23:01 +0900

    스타 수열

--- programmers/Practice/Level3/StarSequence.kt
+++ programmers/Practice/Level3/StarSequence.kt
@@ -6,15 +6,15 @@ class StarSequence {
         for (index in a.indices) {
             val num = a[index]
             val (lastIndex, cnt) = map[num] ?: (-1 to 0)
-            if (lastIndex >= index) {
+            if (lastIndex >= index) { // 1. 집합 쌍을 이룰 수 없음.
                 continue
-            } else if (lastIndex < index - 1 && index > 0 && a[index - 1] != num) {
+            } else if (lastIndex < index - 1 && index > 0 && a[index - 1] != num) { // 2. 왼쪽 원소와 집합 쌍 가능.
                 map[num] = index to (cnt + 1)
-            } else if (index < a.size - 1 && a[index + 1] != num) {
+            } else if (index < a.size - 1 && a[index + 1] != num) { // 3. 오른쪽 원소와 집합 쌍 가능.
                 map[num] = (index + 1) to (cnt + 1)
             }
         }
-        return map.values.maxOf { it.second * 2 }
+        return map.values.maxOf { it.second } * 2
     }
 }
 
