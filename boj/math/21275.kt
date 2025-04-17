66aa84b44e0cf52b709c4d9ab50a3942d9902c38
rio319-an <78468001+rio319-an@users.noreply.github.com>
2025-04-17 15:45:03 +0900

    폰 호석만 21275

--- boj/math/21275.kt
+++ boj/math/21275.kt
@@ -3,89 +3,58 @@ package boj.math
 import java.util.StringTokenizer
 
 class `21275` {
-    private data class Case(val result: String) {
-        val arr = IntArray(result.length) { result[it].toNum() }
-        val minRadix = maxOf(2, arr.max() + 1)
-
-        private fun Char.toNum(): Int {
-            return if (isDigit()) this - '0'
-            else this - 'a' + 10
-        }
-    }
-
-    private val xRadixMap = mutableMapOf<Long, MutableList<Int>>()
-
-    private fun putMap(case: Case) {
-        for (radix in case.minRadix..36) {
-            val x = getX(case, radix)
-            if (x < 0) continue
-            if (xRadixMap[x] == null) {
-                xRadixMap[x] = mutableListOf()
-            }
-            xRadixMap[x]!! += radix
-        }
-    }
-
-    private fun getResult(case: Case): String {
-        var resultX = -1L
-        var a = -1
-        var b = -1
-        for (radix in case.minRadix..36) {
-            val x = getX(case, radix)
-            if (x < 0 || xRadixMap.containsKey(x).not()) continue
-            when (xRadixMap[x]!!.size) {
-                1 -> if (xRadixMap[x]!!.first() != radix) {
-                    if (resultX > 0) return "Multiple"
-                    resultX = x
-                    a = xRadixMap[x]!!.first()
-                    b = radix
-                }
-                2 -> {
-                    val radixList = xRadixMap[x]!!
-                    if (radixList.contains(radix)) {
-                        if (resultX > 0) return "Multiple"
-                        resultX = x
-                        a = if (radixList[0] != radix) radixList[0] else radixList[1]
-                        b = radix
-                    } else {
-                        return "Multiple"
-                    }
-                }
-                else -> return "Multiple"
-            }
-        }
-        return if (resultX < 0) "Impossible"
-        else "$resultX $a $b"
-    }
-
-    private fun getX(case: Case, radix: Int): Long {
+    private fun String.getX(radix: Int): Long {
+        var idx = lastIndex
         var x = 0L
         var base = 1L
-        for (i in case.arr.lastIndex downTo 0) {
-            x += case.arr[i] * base
-            base *= radix
+        while (idx >= 0) {
+            x += this[idx].toNum() * base
             if (x < 0) {
-                return x
+                return -1
             }
+            base *= radix
+            idx--
         }
         return x
     }
 
+    private fun String.getMinRadix(): Int {
+        var maxNum = 0
+        for (ch in this) {
+            maxNum = maxOf(maxNum, ch.toNum())
+        }
+        return maxOf(2, maxNum + 1)
+    }
+
+    private fun Char.toNum(): Int {
+        return if (isDigit()) this - '0'
+        else this - 'a' + 10
+    }
+
     fun solution() = with(System.`in`.bufferedReader()) {
         val st = StringTokenizer(readLine(), " ")
-        val aCase = Case(st.nextToken())
-        val bCase = Case(st.nextToken())
-        val bigCase: Case
-        val smallCase: Case
-        if (aCase.minRadix >= bCase.minRadix) {
-            bigCase = aCase
-            smallCase = bCase
-        } else {
-            bigCase = bCase
-            smallCase = aCase
+        val aResult = st.nextToken()
+        val bResult = st.nextToken()
+        val resultList = mutableListOf<Triple<Long, Int, Int>>()
+        for (aRadix in aResult.getMinRadix()..36) {
+            for (bRadix in bResult.getMinRadix()..36) {
+                if (aRadix == bRadix) continue
+                val aX = aResult.getX(aRadix)
+                val bX = bResult.getX(bRadix)
+                if (aX < 0 || bX < 0) continue
+                if (aX == bX) {
+                    resultList += Triple(aX, aRadix, bRadix)
+                }
+            }
         }
-        putMap(bigCase)
-        val result = getResult(smallCase)
+        val result = if (resultList.isEmpty()) {
+            "Impossible"
+        } else if (resultList.size > 1) {
+            "Multiple"
+        } else {
+            val (x, a, b) = resultList.first()
+            "$x $a $b"
+        }
         print(result)
     }
 }
